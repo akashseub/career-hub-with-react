463e3198df65534a43e354d4655cd08ae5ac1db9
akashseub <miahmdakash55@gmail.com>
2023-10-25 00:28:15 +0600

    first commit

---
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,11 @@
+
+
+const AppliedJobs = () => {
+    return (
+        <div>
+            <h1>Hello from Applied Jobs</h1>            
+        </div>
+    );
+};
+
+export default AppliedJobs;
---
+++ src/main.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
+import Statistics from './Components/Statistics/Statistics';
+import AppliedJobs from './Components/AppliedJobs/AppliedJobs';
+import Blogs from './Components/Blogs/Blogs';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>
+      },
+      {
+        path: "/applied",
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: "/blogs",
+        element: <Blogs></Blogs>
+      }
+    ]
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
